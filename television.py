dd0c920a11011e6e3161337b95ae44f32dcd37c8
Connor McNeel <connormcneel04@gmail.com>
2023-11-17 17:59:29 -0600

    Final Commit for tv

--- television.py
+++ television.py
@@ -7,23 +7,26 @@ class Television:
     def __init__(self):
         self.__status = False
         self.__muted = False
-        self.__volume = self.MIN_VOLUME
-        self.__channel = self.MIN_CHANNEL
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+        self.__temp_volume = 0
 
     def power(self):
-        if self.__status is False:
-            self.__status = True
-        elif self.__status is True:
-            self.__status = False
+        self.__status = not self.__status
+        self.__muted = False
 
     def mute(self):
-        if self.__muted is False:
-            self.__muted = True
-        elif self.__muted is True:
-            self.__muted = False
+        if self.__status:
+            if self.__muted is False:
+                self.__temp_volume = self.__volume
+                self.__volume = 0
+                self.__muted = True
+            elif self.__muted:
+                self.__volume = self.__temp_volume
+                self.__muted = False
 
     def channel_up(self):
-        if self.__status is True:
+        if self.__status:
             if self.__channel == self.MAX_CHANNEL:
                 self.__channel = self.MIN_CHANNEL
             else:
@@ -37,4 +40,32 @@ class Television:
                 self.__channel -= 1
 
     def volume_up(self):
-        pass
+        if self.__status:
+            if self.__muted is True:
+                self.mute()
+                if self.__volume == self.MAX_VOLUME:
+                    pass
+                else:
+                    self.__volume += 1
+            elif self.__muted is False:
+                if self.__volume == self.MAX_VOLUME:
+                    pass
+                else:
+                    self.__volume += 1
+
+    def volume_down(self):
+        if self.__status is True:
+            if self.__muted is True:
+                self.mute()
+                if self.__volume == self.MIN_VOLUME:
+                    pass
+                else:
+                    self.__volume -= 1
+            elif self.__muted is False:
+                if self.__volume == self.MIN_VOLUME:
+                    pass
+                else:
+                    self.__volume -= 1
+
+    def __str__(self):
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
