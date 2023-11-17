7948ec3218a4f92552117f86331d23032a570c1a
Connor McNeel <connormcneel04@gmail.com>
2023-11-17 15:48:26 -0600

    first commit

---
+++ television.py
@@ -0,0 +1,37 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = self.MIN_VOLUME
+        self.__channel = self.MIN_CHANNEL
+
+    def power(self):
+        if self.__status is False:
+            self.__status = True
+        elif self.__status is True:
+            self.__status = False
+
+    def mute(self):
+        if self.__muted is False:
+            self.__muted = True
+        elif self.__muted is True:
+            self.__muted = False
+
+    def channel_up(self):
+        if self.__status is True:
+            if self.__channel == self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
+            else:
+                self.__channel += 1
+
+    def channel_down(self):
+        if self.__status is True:
+            if self.__channel == self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+            else:
+                self.__channel -= 1
